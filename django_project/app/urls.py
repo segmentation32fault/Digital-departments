7b3043fc6c83fa0e09798b80f2427e1a23bff9ed
Ramilische <ramilkasharipov@gmail.com>
2025-05-12 22:53:38 +0300

    RAMILISCHE Реализовал наследование шаблонов, динамически отображаемую навигацию

--- django_project/app/urls.py
+++ django_project/app/urls.py
@@ -3,14 +3,17 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    re_path(r'likes/(?P<username>\w+)/', views.liked_recipes, name='likes'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    re_path(r'likes/(?P<username>\w+)/', views.liked_recipes, name='likes'),
 
     path('popular/', views.popular_recipes, name='popular_recipes'),
+    path('search/', views.search, name='search'),
     re_path(r'^recipe/(?P<num>\d+)/', views.recipe, name='recipe'),
+    path('addrecipe/', views.add_recipe, name='add_recipe'),
 
     path('analytics/', views.analytics, name='analytics'),
+
     path('home/', views.index, name='home'),
     path('', views.index),
 ]
--- django_project/app/views.py
+++ django_project/app/views.py
@@ -12,38 +12,52 @@ from .models import Post, Tag, User
 from .serializers import PostSerializer, TagSerializer, UserSerializer, RegistrationSerializer
 
 
+menu = [{'title': 'Популярное', 'url': 'popular_recipes'},
+        {'title': 'Вход', 'url': 'login'},
+        {'title': 'Регистрация', 'url': 'register'},
+        ]
+
 # Create your views here.
-
-
-def index(request: HttpRequest):
-    return redirect('popular_recipes')
 
 
 def profile(request: HttpRequest):
     return HttpResponse('<h1>Профиль</h1>')
 
+def liked_recipes(request: HttpRequest, username):
+    context = {'liked_recipes': get_liked_recipes(username=username), 'menu': menu}
+    return render(request, 'app/liked.html', context=context)
+
 
 def login(request: HttpRequest):
-    return render(request, 'app/login.html')
+    return render(request, 'app/login.html', context={'menu': menu})
 
 
 def register(request: HttpRequest):
-    return render(request, 'app/register.html')
+    return render(request, 'app/register.html', context={'menu': menu})
 
 
 def popular_recipes(request: HttpRequest):
     recipes = get_popular_recipes(9)
-    return render(request, 'app/index.html', context={'recipes': recipes})
+    return render(request, 'app/index.html', context={'recipes': recipes, 'menu': menu})
+
+
+def search(request: HttpRequest):
+    # return render(request, 'app/search.html')
+    return HttpResponse('<h1>Поиск рецепта</h1>')
 
 
 def recipe(request: HttpRequest, num=1):
-    recipe_info = find_recipe(int(num))
-
-    if recipe_info is None:
+    context = find_recipe(int(num))
+    if context is None:
         #   если рецепт не нашелся, то перенаправление на гл страницу или страницу ошибки
         return redirect('popular_recipes')
 
-    return render(request, 'app/recipe.html', context=recipe_info)
+    context.update({'menu': menu})
+    return render(request, 'app/recipe.html', context=context)
+
+
+def add_recipe(request: HttpRequest):
+    return HttpResponse('<h1>Добавление рецепта</h1>')
 
 
 def analytics(request: HttpRequest):
@@ -51,9 +65,10 @@ def analytics(request: HttpRequest):
     return HttpResponse(f'<h1>Временная аналитика</h1>\n<p>Host: {host}</p>')
 
 
-def liked_recipes(request: HttpRequest, username):
-    context = {'liked_recipes': get_liked_recipes(username=username)}
-    return render(request, 'app/liked.html', context=context)
+def index(request: HttpRequest):
+    return redirect('popular_recipes')
+
 
 def page_not_found(request: HttpRequest, exception):
+    context = {'menu': menu}
     return render(request, 'app/404.html', status=404)
